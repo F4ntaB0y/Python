81c2a4477ec8a90d3cab0ee1a6a309c2325a6cbc
F4ntaBoy <aryapangestu2024@gmail.com>
2025-01-29 00:31:13 +0700

    push polaSegitigaTerbalik.py

---
+++ polaBintang/polaSegitigaTerbalik.py
@@ -0,0 +1,16 @@
+print('## Program Python Segitiga Bintang Terbalik ##')
+print('============================================')
+print()
+
+tinggiSegitiga = int(input('Masukan tinggi segitiga: '))
+print()
+
+# ?dua perulangan
+# for i in range(tinggiSegitiga):
+#     for j in range(tinggiSegitiga - i):
+#         print(' *', end="")
+#     print()
+
+# ?satu perulangan
+for i in range(tinggiSegitiga):
+    print(' *' * (tinggiSegitiga - i))
