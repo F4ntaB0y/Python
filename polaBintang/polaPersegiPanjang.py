8ecaa802369f01ec84d448e45ee0edf003f37ecb
F4ntaBoy <aryapangestu2024@gmail.com>
2025-01-29 00:30:03 +0700

    Push polaPersegiPanjang.py

---
+++ polaBintang/polaPersegiPanjang.py
@@ -0,0 +1,17 @@
+print('## Program Python Persegi Panjang Bintang ##')
+print('============================================')
+print()
+
+tinggi = int(input("Masukan tinggi persegi: "))
+lebar = int(input("Masukan lebar persegi : "))
+print()
+
+# ?dua perulangan
+# for i in range(tinggi):
+#     for i in range(lebar):
+#         print(' *', end='')
+#     print()
+
+# ?satu perulangan
+for i in range(tinggi):
+        print(' *' * lebar)
