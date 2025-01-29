ee8a299786e88648998e64f995defc9e0c94d107
Aestu <aryapangestu2024@gmail.com>
2025-01-29 22:23:39 +0700

    Update polaPersegiPanjang.py

--- polaBintang/polaPersegiPanjang.py
+++ polaBintang/polaPersegiPanjang.py
@@ -6,12 +6,12 @@ tinggi = int(input("Masukan tinggi persegi: "))
 lebar = int(input("Masukan lebar persegi : "))
 print()
 
-# ?dua perulangan
+#? dua perulangan
 # for i in range(tinggi):
 #     for i in range(lebar):
 #         print(' *', end='')
 #     print()
 
-# ?satu perulangan
+#? satu perulangan
 for i in range(tinggi):
         print(' *' * lebar)
