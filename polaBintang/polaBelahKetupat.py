464efe62f746defa0a620254bd59a189132dc88b
F4ntaBoy <aryapangestu2024@gmail.com>
2025-01-29 16:20:58 +0700

    Update polaBelahKetupat.py

--- polaBintang/polaBelahKetupat.py
+++ polaBintang/polaBelahKetupat.py
@@ -5,19 +5,28 @@ print()
 lebarBelahKetupat = int(input('Masukan lebar belah ketupat: '))
 print()
 
-# ?enam perulangan
+# # ?enam perulangan
+# for i in range(lebarBelahKetupat):
+#     for j in range(lebarBelahKetupat - i):
+#         print(' ', end='')
+
+#     for k in range(i + 1):
+#         print(' *',end='')
+#     print()
+
+# for i in range(1, lebarBelahKetupat): # !Set agar perulangan mulai dari 1
+#     for j in range(i + 1):
+#         print(' ', end='')
+
+#     for k in range(lebarBelahKetupat - i):
+#         print(' *', end='')
+#     print()
+
+# ?dua perulangan
 for i in range(lebarBelahKetupat):
-    for j in range(lebarBelahKetupat - i):
-        print(' ', end='')
+    print(' ' * (lebarBelahKetupat - i), end='')
+    print(' *' * (i + 1))
 
-    for k in range(i + 1):
-        print(' *',end='')
-    print()
-
-for i in range(1, lebarBelahKetupat): # !Set agar perulangan mulai dari 1
-    for j in range(i + 1):
-        print(' ', end='')
-
-    for k in range(lebarBelahKetupat - i):
-        print(' *', end='')
-    print()
+for i in range(1, lebarBelahKetupat):
+    print(' ' * (i + 1), end='')
+    print(' *' * (lebarBelahKetupat - i))
