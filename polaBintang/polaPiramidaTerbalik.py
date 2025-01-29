d4f3f9954649b30425e79e2462916f2bdb2dfaba
F4ntaBoy <aryapangestu2024@gmail.com>
2025-01-29 15:53:20 +0700

    push polaPiramidaTerbalik.py

---
+++ polaBintang/polaPiramidaTerbalik.py
@@ -0,0 +1,20 @@
+print('## Program Python Piramida Bintang Terbalik ##')
+print('============================================')
+print()
+
+tinggiPiramida = int(input('Masukan tinggi piramaida: '))
+print()
+
+# ?dua perulangan
+# for i in range(tinggiPiramida):
+#     for j in range(i + 1):
+#         print(' ', end='')
+    
+#     for k in range(tinggiPiramida-i):
+#         print(' *', end='')
+#     print()
+
+# ?satu perulangan
+for i in range(tinggiPiramida):
+    print(' ' * (i + 1), end='')
+    print(' *' * (tinggiPiramida - i))
