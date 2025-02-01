9bb935288c40592f1c072eac83bea17fcc2e8426
Aestu <aryapangestu2024@gmail.com>
2025-02-01 19:22:33 +0700

    push membuatDeretAngka.py

---
+++ deretAngka/membuatDeretAngka.py
@@ -0,0 +1,30 @@
+print('##  Program Python Deret Angka  ##')
+print('==================================')
+print()
+
+#! input
+jumlahDeret = int(input('Jumlah deret yang diinginkan: '))
+
+#? versi(A) naik 1 angka
+# for i in range(1, jumlahDeret + 1):
+#     print(i, end=' ')
+
+#? versi(A.1) naik 1 angka
+# for i in range(jumlahDeret):
+#     print(i + 1, end=' ')
+
+#? versi(B) kelipatan 3
+# for i in range(1, jumlahDeret + 1):
+#     print(i * 3, end=' ')
+
+#? versi(C) mulai dari 6
+# for i in range(1, jumlahDeret + 1):
+#     print(i + 5, end=' ')
+
+#? versi(C.1) mulai dari 6
+# for i in range(jumlahDeret):
+#     print(i + 6, end=' ')
+
+#? versi(D)
+# for i in range(1, jumlahDeret + 1):
+#     print(i * i, end=' ')
