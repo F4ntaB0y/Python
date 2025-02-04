df5287bee8c1d3b4e8d2dd9ff29b4f85d0e35d85
Aestu <aryapangestu2024@gmail.com>
2025-02-04 23:22:43 +0700

    Update cekBilanganPrima.py

--- logikaMTK/cekBilanganPrima.py
+++ logikaMTK/cekBilanganPrima.py
@@ -15,7 +15,7 @@ print()
 # else:
 #     #? Memeriksa apakah angka memiliki pembagi selain 1 dan dirinya sendiri
 #     # for i in range(2, (x // 2)):  #! Loop dari 2 hingga x//2 (kurang optimal)
-#     for i in range(2, int(x ** 0.5) + 1): #* gunakan agar lebih cepat:
+#     for i in range(2, int(x ** 0.5) + 1): #* gunakan agar lebih cepat, karena faktor bilangan tidak mungkin lebih besar dari akar kuadratnya.
 #         if ((x % i) == 0):  # Jika habis dibagi i, maka bukan bilangan prima
 #             angka_prima = False
 #             break  # Hentikan loop jika sudah ditemukan faktor pembagi
@@ -28,24 +28,4 @@ print()
 
 #* versi(2)
 #? Meminta input dari pengguna berupa angka bulat
-x = int(input('Input satu angka bulat: '))
-
-#? Mengasumsikan bahwa angka tersebut adalah bilangan prima
-angka_prima = True
-
-#? Jika angka adalah 0 atau 1, maka bukan bilangan prima
-if (x == 0) or (x == 1):
-    angka_prima = False
-else:
-    #? Loop untuk memeriksa apakah angka memiliki pembagi selain 1 dan dirinya sendiri
-    # for i in range(2, (x // 2)):  #! Loop dari 2 hingga x//2 (tidak optimal)
-    for i in range(2, int(x ** 0.5) + 1): #* gunakan agar lebih cepat:
-        if (x % i) == 0:  # Jika x habis dibagi i, maka bukan bilangan prima
-            angka_prima = False
-            break  # Hentikan loop setelah menemukan pembagi
-
-#? Menampilkan hasil apakah angka prima atau bukan
-if angka_prima:
-    print(x, 'adalah angka prima')
-else:
-    print(x, 'bukan angka prima, karena bisa dibagi', i)  # Menampilkan pembagi pertama yang ditemukan
+#
