5336339ca38a18d03826bf70bfd18e79271aa01c
Aestu <aryapangestu2024@gmail.com>
2025-02-05 10:03:06 +0700

    Bug Fix cekBilanganPrima.py
    BUG IN VERSION 1
    - Name Error if entered 1 or 0
    - If entered 4 into x the result is prime even though 4 is not prime

--- logikaMTK/cekBilanganPrima.py
+++ logikaMTK/cekBilanganPrima.py
@@ -9,48 +9,46 @@ Bilangan prima adalah bilangan yang hanya memiliki dua faktor(1 dan dirinya send
 0 dan 1 bukan bilangan prima.
 """
 
-#* versi(1)
-x = int(input('Input satu angka bulat: '))
-
-angka_prima = True
-
-#? 0 atau 1 bukan bilangan prima,
-# 0 bukan prima karena bisa dibagi dengan semua bilangan,
-# 1 karena hanya bisa dibagi dengan 1.
-if ((x == 0) or (x == 1)):
-    angka_prima = False
-else:
-    #? Memeriksa apakah angka memiliki pembagi selain 1 dan dirinya sendiri
-    for i in range(2, (x // 2)):
-    # for i in range(2, int(x ** 0.5) + 1): #* Alternativ loop(lebih efisien), karena faktor bilangan tidak mungkin lebih besar dari akar kuadratnya.
-        if ((x % i) == 0): 
-            angka_prima = False
-            break
-if angka_prima:
-    print(x, 'adalah angka prima')
-else:
-    print(x, 'bukan angka prima')
-
-#* versi(2)
-# #? Meminta input dari pengguna berupa angka bulat
+# #* versi(1)
 # x = int(input('Input satu angka bulat: '))
 
-# #? Mengasumsikan bahwa angka tersebut adalah bilangan prima
 # angka_prima = True
 
-# #? Jika angka adalah 0 atau 1, maka bukan bilangan prima
-# if (x == 0) or (x == 1):
+# #? 0 atau 1 bukan bilangan prima,
+# # 0 bukan prima karena bisa dibagi dengan semua bilangan,
+# # 1 karena hanya bisa dibagi dengan 1.
+# if ((x == 0) or (x == 1)):
 #     angka_prima = False
 # else:
-#     #? Loop untuk memeriksa apakah angka memiliki pembagi selain 1 dan dirinya sendiri
-#     # for i in range(2, (x // 2)):  #! Loop dari 2 hingga x//2 (tidak optimal)
-#     for i in range(2, int(x ** 0.5) + 1): #* gunakan agar lebih cepat:
-#         if (x % i) == 0:  # Jika x habis dibagi i, maka bukan bilangan prima
+#     #? Memeriksa apakah angka memiliki pembagi selain 1 dan dirinya sendiri
+#     for i in range(2, (x // 2)):
 #             angka_prima = False
-#             break  # Hentikan loop setelah menemukan pembagi
-
-# #? Menampilkan hasil apakah angka prima atau bukan
+#             break
 # if angka_prima:
 #     print(x, 'adalah angka prima')
 # else:
-#     print(x, 'bukan angka prima, karena bisa dibagi', i)  # Menampilkan pembagi pertama yang ditemukan
+#     print(x, 'bukan angka prima karena bisa dibagi', i)
+
+"""
+#* BUG DI VERSI 1
+#! Name Error jika dimasaukan 1 atau 0
+#! Jika memasukan 4 ke x hasilnya prima pdhl 4 bukan prima
+"""
+
+#* versi(2) memperbaiki bug di versi sebelumnya
+x = int(input('Input satu angka bulat: '))
+
+if x < 2:
+    print(x, 'bukan angka prima')
+else:
+    angka_prima = True 
+
+    for i in range(2, int(x**0.5) + 1): #? faktor bilangan tidak mungkin lebih besar dari akar kuadratnya.
+        if x % i == 0:  
+            angka_prima = False
+            print(x, 'bukan angka prima, karena bisa dibagi', i)  # Cetak pembagi pertama yang ditemukan
+            break  
+
+    # Jika tidak ditemukan pembagi, berarti x adalah bilangan prima
+    if angka_prima:
+        print(x, 'adalah angka prima')
