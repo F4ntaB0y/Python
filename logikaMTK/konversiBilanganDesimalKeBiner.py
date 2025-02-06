c390a48630e865daf9bbb624f8fc2a791e9611e9
Aestu <aryapangestu2024@gmail.com>
2025-02-07 06:52:09 +0700

    Update konversiBilanganDesimalKeBiner.py

--- logikaMTK/konversiBilanganDesimalKeBiner.py
+++ logikaMTK/konversiBilanganDesimalKeBiner.py
@@ -4,37 +4,32 @@ print('================================================')
 print()
 
 #* versi(1)
-# Meminta pengguna untuk memasukkan angka desimal
 n = int(input('Input angka desimal: '))
 
-# List untuk menyimpan hasil konversi biner
-a = []
-i = 0
+a = [] # List untuk menyimpan hasil konversi biner
+i = 0 # berfungsi untuk proses increment perulangan while.
 
-# Mengonversi angka desimal ke biner menggunakan metode pembagian berulang
+#? Mengonversi angka desimal ke biner menggunakan metode pembagian berulang
 while n > 0:
-    a.append(n % 2)  # Menyimpan sisa hasil bagi (0 atau 1)
+    a.append(n % 2)  # Menyimpan sisa hasil bagi (0 atau 1) ke list a
     n = n // 2  # Membagi angka dengan 2 untuk mendapatkan nilai berikutnya
-    i = i + 1  # Menambah indeks
+    i = i + 1  # Menambah indeks 
 
-# Menampilkan hasil konversi dalam format biner
 print('Angka binernya adalah: ', end='')
 
 # Membaca list dari belakang untuk menampilkan angka biner dengan benar
 for i in range(i - 1, -1, -1):
-    print(a[i], end='')  # Mencetak angka biner tanpa spasi tambahan
+    print(a[i], end='')  # Mencetak angka biner sesuai index dana tanpa spasi tambahan
 
 #* versi(2)
-# Meminta pengguna untuk memasukkan angka desimal
-n = int(input('Input angka desimal: '))
+# n = int(input('Input angka desimal: '))
 
-# Mengonversi angka desimal ke biner menggunakan fungsi bawaan bin()
-# Fungsi bin() menghasilkan string dalam format '0b...' sehingga '0b' dihapus dengan replace()
-biner = bin(n).replace("0b", "")
+# # Mengonversi angka desimal ke biner menggunakan fungsi bawaan bin()
+# # Fungsi bin() menghasilkan string dalam format '0b...' sehingga '0b' dihapus dengan replace()
+# biner = bin(n).replace("0b", "")
 
-# Menampilkan hasil konversi dalam format biner
-print('Angka binernya adalah:', biner)
+# print('Angka binernya adalah:', biner)
 
-#! Berikut contoh perbedaan hasil dari bin(x) dan bin(x).replace("0b",""):
-print(bin(211))                   ## 0b11010011
-print(bin(211).replace("0b",""))  ## 11010011
+# #? Berikut contoh perbedaan hasil dari bin(x) dan bin(x).replace("0b",""):
+# print(bin(211))                   # 0b11010011
+# print(bin(211).replace("0b",""))  # 11010011
