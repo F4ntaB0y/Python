e018ff91c130a85498b8fcf8da1574868e01f5cb
Aestu <aryapangestu2024@gmail.com>
2025-02-16 02:47:59 +0700

    Update konversiBinerKeDesimal.py

--- logikaMTK/konversiBinerKeDesimal.py
+++ logikaMTK/konversiBinerKeDesimal.py
@@ -6,11 +6,10 @@ print()
 angka_biner = int(input('Input angka biner: '))
 print('Angka desimal dari biner', angka_biner, end='')
 
-# Inisialisasi variabel untuk menyimpan hasil konversi
-angka_desimal = 0  # Menyimpan hasil akhir dalam bentuk desimal
-i = 1  # Nilai tempat (1, 2, 4, 8, ... sesuai dengan basis 2)
+angka_desimal = 0
+i = 1 
 
-# Proses konversi biner ke desimal
+#? Proses konversi biner ke desimal
 while (angka_biner != 0):
     digit = angka_biner % 10  # Mengambil digit terakhir dari angka biner
     angka_desimal = angka_desimal + (digit * i)  # Mengalikan digit dengan nilai tempatnya
@@ -20,10 +19,12 @@ while (angka_biner != 0):
 print(' =', angka_desimal)
 
 #* versi(2) cara singkat dengan function bin()
-angka_biner = input('Input angka biner: ')
+# # Meminta pengguna untuk memasukkan angka biner dalam bentuk string
+# angka_biner = input('Input angka biner: ')
 
-# Mengonversi angka biner ke desimal menggunakan fungsi bawaan int()
-# Parameter kedua pada int() adalah basis bilangan (dalam hal ini basis 2)
-angka_desimal = int(angka_biner, 2)
+# # Mengonversi angka biner ke desimal menggunakan fungsi bawaan int()
+# # Parameter kedua pada int() adalah basis bilangan (dalam hal ini basis 2)
+# angka_desimal = int(angka_biner, 2)
 
-print('Angka desimal dari biner', angka_biner, '=', angka_desimal)
+# # Menampilkan hasil konversi dari biner ke desimal
+# print('Angka desimal dari biner', angka_biner, '=', angka_desimal)
