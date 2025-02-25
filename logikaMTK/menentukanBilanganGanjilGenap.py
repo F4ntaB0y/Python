c587275b7e29c9d438dd211e0be25170bac176d6
Aestu <aryapangestu2024@gmail.com>
2025-02-25 21:04:36 +0700

    Update menentukanBilanganGanjilGenap.py

--- logikaMTK/menentukanBilanganGanjilGenap.py
+++ logikaMTK/menentukanBilanganGanjilGenap.py
@@ -3,19 +3,19 @@ print('=====================================================')
 print()
 
 #* versi(1)
-x = int(input('Input sebuah angka bulat: '))
+# x = int(input('Masukan sebuah angka bulat: '))
 
-# Mengecek apakah angka genap atau ganjil
-if (x % 2 == 0):
-    print(x, 'adalah bilangan genap')
-else:  
-    print(x, 'adalah bilangan ganjil')
+# # Mengecek apakah angka genap atau ganjil
+# if(x % 2 == 0):
+#     print(x, 'adalah angka genap')
+# else:
+#     print(x, 'adalah angka ganjil')
 
 #* versi(2)
-x = int(input('Input sebuah angka bulat: '))
+# x = int(input('Masukan angka bulat'))
 
-# Mengecek apakah angka ganjil atau genap dengan kondisi terbalik
-if (x % 2 != 0):  
-    print(x, 'adalah bilangan ganjil')
-else:  
-    print(x, 'adalah bilangan genap')
+# # Mengecek apakah angka ganjil atau genap dengan kondisi terbalik
+# if(x % 2 != 0):
+#     print(x, 'adalah angka ganjil')
+# else:
+#     print(x, 'adalah angka genap')
