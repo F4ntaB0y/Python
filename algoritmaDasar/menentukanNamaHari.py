8f846ba1bb9b281afb331ed1b034f12f12f5a418
Aestu <aryapangestu2024@gmail.com>
2025-02-27 20:46:02 +0700

    Push menentukanNamaHari.py

--- algoritmaDasar/menentukanNamaHari.py
+++ algoritmaDasar/menentukanNamaHari.py
@@ -2,21 +2,46 @@ print('##  Program Python Menentukan Nama Hari  ##')
 print('===========================================')
 print()
 
-x = int(input("Masukan angka hari (1-7): "))
+#* versi(1)
+# x = int(input("Masukan angka hari (1-7): "))
 
-if(x == 1):
-    print("Senin")
-elif(x == 2):
-    print("Selasa")
-elif(x == 3):
-    print("Rabu")
-elif(x == 4):
-    print("Kamis")
-elif(x == 5):
-    print("Jumat")
-elif(x == 6):
-    print("Sabtu")
-elif(x == 7):
-    print("Minggu")
-else:
-    print("Pilihan tidak tersedia")
+# if(x == 1):
+#     print("Senin")
+# elif(x == 2):
+#     print("Selasa")
+# elif(x == 3):
+#     print("Rabu")
+# elif(x == 4):
+#     print("Kamis")
+# elif(x == 5):
+#     print("Jumat")
+# elif(x == 6):
+#     print("Sabtu")
+# elif(x == 7):
+#     print("Minggu")
+# else:
+#     print("Pilihan tidak tersedia")
+
+#* versi(2)
+# x = int(input("Masukan angka hari (1-7): "))
+
+# match x:
+#     case 1:
+#         hari = "Senin"
+#     case 2:
+#         hari = "Selasa"
+#     case 3:
+#         hari = "Rabu"
+#     case 4:
+#         hari = "Kamis"
+#     case 5:
+#         hari = "Jumat"
+#     case 6:
+#         hari = "Sabtu"
+#     case 7:
+#         hari = "Minggu"
+#     case _:
+#         print("Pilihan tidak tersedia")
+    
+# if((x >= 1) and (x <= 7)):
+#     print('Hari ke-', x, ' adalah ', hari, sep='')
