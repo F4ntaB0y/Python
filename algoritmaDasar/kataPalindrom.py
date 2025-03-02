93b70e8c8f004fec276ee0d40529f0343754b2e0
Aestu <aryapangestu2024@gmail.com>
2025-03-02 11:15:36 +0700

    Update kataPalindrom.py
    Menambahkan komentar penjelas

--- algoritmaDasar/kataPalindrom.py
+++ algoritmaDasar/kataPalindrom.py
@@ -1,16 +1,22 @@
+#* Menampilkan judul program
 print('##  Program Python Cek Kata Palindrom  ##')
 print('=========================================')
 print()
 
+#* Menerima input dari pengguna
 x = input('Masukan kata/kalimat: ')
-palindrom = True
-panjang_x = len(x)
 
-for i in range(panjang_x // 2):
-    if(x[i] != x[panjang_x-i-1]):
-        palindrom = False
-        break
+#* Inisialisasi variabel
+palindrom = True # Asumsi awal bahwa input adalah palindrom
+panjang_x = len(x) # Menghitung panjang string
 
+#* Mengecek apakah input adalah palindrom
+for i in range(panjang_x // 2): # Looping setengah panjang string
+    if(x[i] != x[panjang_x-i-1]): # Bandingkan karakter depan dan
+        palindrom = False # Jika berbeda, bukan palindrom
+        break # Hentikan loop jika bukan palindrom
+
+#* Menampilkan hasil
 if palindrom:
     print(x, 'adalah palindrome!')
 else:
