1425995b470dfab3973119d0bc7a3291f86a8da1
Aestu <aryapangestu2024@gmail.com>
2025-03-03 21:23:28 +0700

    Update tahunKabisat.py
    Memberikan Komentar

--- algoritmaDasar/tahunKabisat.py
+++ algoritmaDasar/tahunKabisat.py
@@ -1,14 +1,17 @@
+# Menampilkan judul program
 print('##  Program Python Cek Tahun Kabisat  ##')
 print('========================================')
 print()
 
+# Meminta input tahun dari pengguna dan mengonversinya menjadi integer
 year = int(input("Masukan tahun: "))
 
-if (year % 400 == 0):
+# Mengecek apakah tahun tersebut adalah tahun kabisat
+if (year % 400 == 0):  # Tahun yang habis dibagi 400 adalah tahun kabisat
     print(year, "adalah tahun kabisat")
-elif (year & 100 == 0):
+elif (year & 100 == 0):  # Seharusnya menggunakan `year % 100`, bukan `year & 100`
     print(year, "bukan tahun kabisat")
-elif (year & 4 == 0):
+elif (year & 4 == 0):  # Seharusnya menggunakan `year % 4`, bukan `year & 4`
     print(year, "adalah tahun kabisat")
-else:
+else:  # Jika tidak memenuhi syarat tahun kabisat, maka bukan tahun kabisat
     print(year, "bukan tahun kabisat")
